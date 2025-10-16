0751f44c02596e457221b73bd818b4e465d23ef9
xoyoc <xoyoc_l2@hotmail.com>
2025-10-15 19:39:57 -0600

    Correcion de settings

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.monkfish-app-zamvc.ondigitalocean.app',
